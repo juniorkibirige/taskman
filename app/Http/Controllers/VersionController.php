25112ad20361a9f61b141a2056abef240d19cf97
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-06-20 17:48:58 +0300

    Refactored for iosVersions and androidVersions

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -11,7 +11,7 @@ class VersionController extends Controller
     {
         $appId = $request['appId'];
         $app = AppVersion::findOrFail($appId);
-        return json_encode(["app_id"=>$appId, "android" => $app->android, "ios"=>$app->ios]);
+        return json_encode(["app_id" => $appId, "android" => $app->android, "ios" => $app->ios]);
     }
 
     public function setVersion(Request $request)
@@ -19,11 +19,19 @@ class VersionController extends Controller
         $appId = $request['appId'];
         $version = $request['android'];
         $iosVersion = $request['ios'];
-        $app = AppVersion::firstOrCreate(['app_id' => $appId, 'android' => $version, 'ios' => $iosVersion]);
-        $app->update([
-            'android' => $version,
-            'ios' => $iosVersion,
-        ]);
+        $app = AppVersion::query()->where(['app_id' => $appId])->get();
+        if ($app->count() > 0)
+            AppVersion::query()->where(['app_id' => $appId])->update([
+                'android' => $version,
+                'ios' => $iosVersion,
+            ]);
+        else {
+            AppVersion::create([
+                'app_id' => $appId,
+                'android' => $version,
+                'ios' => $iosVersion,
+            ]);
+        }
         return "Successfully Updated";
     }
 }
