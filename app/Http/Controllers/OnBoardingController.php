b59371845b307db6ef34b15feb2051f38d418585
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-08-07 19:36:21 +0300

    Added onboarding controller

---
+++ app/Http/Controllers/OnBoardingController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OnBoarding;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class OnBoardingController extends Controller
+{
+    public function getOnBoardingStatus(Request $request): JsonResponse
+    {
+        $appId = $request['app_id'];
+        $userId = $request['user_id'];
+        $app = OnBoarding::query()
+            ->where(['app_id' => $appId])
+            ->where(['user_id' => $userId])
+            ->firstOrFail();
+        return response()->json(['status' => (boolean) $app['status']]);
+    }
+
+    public function setOnBoardingStatus(Request $request): string
+    {
+        $appId = $request['app_id'];
+        $userId = $request['user_id'];
+        $status = $request['status'];
+        OnBoarding::create([
+                'status' => $status,
+                'user_id' => $userId,
+                'app_id' => $appId,
+            ]);
+        return response(null, 204);
+    }
+}
