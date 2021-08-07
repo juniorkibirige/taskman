315848c5e458c76d567f5f6a2e52c57568593081
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-08-07 19:36:14 +0300

    Added onboarding model

---
+++ app/OnBoarding.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OnBoarding extends Model
+{
+    protected $table = 'onboarding';
+
+    protected $fillable = [
+        'user_id',
+        'status'
+    ];
+}
